fe6fe7a91ec483169b23cdc177304a17e76bd0bb
binZuy <duynguyenminh0206@gmail.com>
2023-12-23 04:44:10 +0700

    Solve conflict

--- src/Functions/Function1.java
+++ src/Functions/Function1.java
@@ -4,9 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
-import ui.fxml.MainScreenController;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,15 +16,10 @@ import java.util.regex.Pattern;
 
 public class Function1 {
 
-	private MainScreenController mainScreenController;
-	
-	public void setMainScreenController(MainScreenController mainScreenController) {
-		this.mainScreenController = mainScreenController;
-	}
+
     public static void main(String[] args) {
     	//Nothing
-    	
-    displayFilteredHashtagBlog("airnft.json","NFTs");
+
     }
     
     
