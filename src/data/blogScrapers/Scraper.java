2c6e209b9efc44a0bb4c20a0367ef39b2e045b5a
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2023-12-23 18:05:10 +0700

    modify scraper

--- src/data/blogScrapers/Scraper.java
+++ src/data/blogScrapers/Scraper.java
@@ -1,14 +1,20 @@
 package data.blogScrapers;
 
+import data.apiFetcher.Fetcher;
+
 public class Scraper {
 
-    public void scrape() {
+    public void scrape(){
+    	Fetcher fetcher = new Fetcher();
         AirnftScraper airnft = new AirnftScraper();
         NfticallyScraper nftically = new NfticallyScraper();
         NFTNewsTodayScraper nftnews = new NFTNewsTodayScraper();
         NonFungibleScraper nonfungible = new NonFungibleScraper();
 
         try {
+        	System.out.println("Fetching API data...");
+        	fetcher.fetch();
+        	
             System.out.println("Scraping Airnft...");
             airnft.scrape();
 
